405dc25a0fb59d25db7071931e3dbb38ef3f8e61
Gen <archiherewego@gmail.com>
2020-08-26 13:53:42 +0900

    JSONのパース処理を削除
    参考にしているサイトとJSONの構造が違うのでそのままでは動きません
    JSONを手動でパースする場合は、構造に合わせてパース処理を書く必要があります
    今回は省略して、APIのレスポンスをそのまま文字列として出力するようにしています

--- app/src/main/java/com/example/skillschallenge/MainActivity.kt
+++ app/src/main/java/com/example/skillschallenge/MainActivity.kt
@@ -110,21 +110,7 @@ class MainActivity : AppCompatActivity() {
                 //先ほどbufferに入れた、取得した文字列
                 val jsonText = buffer.toString()
 
-                //JSONObjectを使って、まず全体のJSONObjectを取ります。
-                val parentJsonObj = JSONObject(jsonText)
-                //今回のJSONは配列になっているので（データは一つですが）、全体のJSONObjectから、getJSONArrayで配列"movies"を取ります。
-                val parentJsonArray = parentJsonObj.getJSONArray("movies")
-
-                //JSONArrayの中身を取ります。映画"Your Name"のデータは、配列"movies"の０番目のデータなので、
-                val detailJsonObj = parentJsonArray.getJSONObject(0)  //これもJSONObjectとして取得
-
-                //moviesの0番目のデータのtitle項目をStringで取ります。これで中身を取れました。
-                val movieName: String = detailJsonObj.getString("title")  // => Your Name.
-                //公開年を取りたい時も同じようにすれば良いです。
-                val year: Int = detailJsonObj.getInt("year")  // => 2016
-
-                //Stringでreturnしてあげましょう。
-                return "$movieName - $year"  // => Your Name. - 2016
+                return jsonText
 
                 //ここから下は、接続エラーとかJSONのエラーとかで失敗した時にエラーを処理する為のものです。
             } catch (e: MalformedURLException) {
