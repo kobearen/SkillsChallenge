d4199c6651004685b5bdd6e1e633317cfddb01fc
satoshi_kimura <>
2020-11-10 23:10:21 +0900

    [update]ボタン押下時の処理実装

--- app/src/main/java/com/example/skillschallenge/MainActivity.kt
+++ app/src/main/java/com/example/skillschallenge/MainActivity.kt
@@ -1,28 +1,13 @@
 package com.example.skillschallenge
 
-
-import android.content.Intent
-import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import android.webkit.WebView
-import androidx.appcompat.app.AlertDialog
-import androidx.core.app.ActivityCompat
-import androidx.core.app.ShareCompat
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONException
-import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.MalformedURLException
-import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,5 +17,35 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.main,menu)
         return true
     }
-
+    override fun onOptionsItemSelected(item: MenuItem?):Boolean{
+        when(item?.itemId) {
+            R.id.home -> {
+                //───①
+                menuImage.setImageResource(R.drawable.okasan)
+                menuText.text = ""
+                return true
+            }
+            R.id.greencurry -> {
+                menuImage.setImageResource(R.drawable.nizakana)
+                menuText.text = getString(R.string.greencurry_text)
+                return true
+            }
+            R.id.katsucurry -> {
+                menuImage.setImageResource(R.drawable.sasimi)
+                menuText.text = getString(R.string.katsucurry_text)
+                return true
+            }
+            R.id.oden -> {
+                menuImage.setImageResource(R.drawable.pizza)
+                menuText.text = getString(R.string.tamoricurry_text)
+                return true
+            }
+            R.id.katsucurry -> {
+                menuImage.setImageResource(R.drawable.surmon)
+                menuText.text = getString(R.string.katsucurry_text)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
