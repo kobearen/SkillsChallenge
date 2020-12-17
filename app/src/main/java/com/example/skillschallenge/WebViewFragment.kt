737bdd839699d54a7dc17a3856d06b3193f1c6c9
satoshi_kimura <>
2020-12-17 13:00:59 +0900

    [update]addJavascriptInterfaceを追加

--- app/src/main/java/com/example/skillschallenge/WebViewFragment.kt
+++ app/src/main/java/com/example/skillschallenge/WebViewFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.fragment_web_view.*
 
-const val NETWORK_ADDRESS ="https://tanukigolf.com/"
-
+//const val NETWORK_ADDRESS ="https://tanukigolf.com/"
+const val NETWORK_ADDRESS = "http://192.168.128.200:8080/#/"
+//const val NETWORK_ADDRESS = "http://192.168.128.233:8081/#/confirm_email"
 class WebViewFragment : Fragment() {
+    private lateinit var mWebView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +31,24 @@ class WebViewFragment : Fragment() {
         //webview内のJavaScriptを有効にする
         mWebView.settings.javaScriptEnabled = true
         mWebView.webViewClient = WebViewClient()
+        mWebView.addJavascriptInterface(this, "Android")
         mWebView.loadUrl(NETWORK_ADDRESS)
         return v
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_load_js.setOnClickListener{
+            loadJS("javascript: isConnectedDevice('braino',true)")
+            loadJS("javascript: isAttachedDevice('braino',true)")
+        }
+    }
+    @JavascriptInterface
+    fun startSensorDataTransfer(recordingId: String) {
+        // デバイスから受信した計測データを計算し始める
+        println("recordingId=$recordingId")
+    }
+    fun loadJS(javascript: String) {
+        // 画面のロードが完了していない場合は画面のロード後にjavascriptを叩く
+        web_view.evaluateJavascript(javascript, null)
     }
 }
